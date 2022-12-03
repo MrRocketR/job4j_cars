61dacacf29743cceb92f04d39ea2cd6d29884c78
MrRocketR <kuptsovns@gmail.com>
2022-12-03 14:44:01 +0300

    2. ManyToMany

--- db/scripts/004_ddl_create_subscription_table.sql
+++ db/scripts/004_ddl_create_subscription_table.sql
@@ -1,5 +1,5 @@
-create table subscription {
+create table subscription (
     id serial primary key,
     s_post_id int not null REFERENCES AUTO_POST(id),
     s_user_id int not null REFERENCES AUTO_USER(id)
-};
+);
