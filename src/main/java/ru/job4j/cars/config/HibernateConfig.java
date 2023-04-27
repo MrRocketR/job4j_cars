699cfc9da49ee2bd9d4d066121c5a3777733ace6
MrRocketR <kuptsovns@gmail.com>
2023-04-27 20:52:50 +0300

    Передалал конфиг

---
+++ src/main/java/ru/job4j/cars/config/HibernateConfig.java
@@ -0,0 +1,27 @@
+package ru.job4j.cars.config;
+
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HibernateConfig {
+
+    @Bean(destroyMethod = "close")
+    public SessionFactory sf() {
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure().build();
+        return new MetadataSources(registry).buildMetadata().buildSessionFactory();
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+}
